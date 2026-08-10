947b879e1232077344acad624c1b79b34182748c
Vinayak Ghosh <r.vinayakghosh@gmail.com>
2026-08-10 14:56:08 +0530

    writing first code in rust

---
+++ Module1 Setup, Primitive Types, Control Flow/module1_practice/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x = 5; // immutable variable
+    println!("x = {}", x);
+
+    let x="five";
+    println!("x = {}", x);
+}
---
+++ Module2 Guessing Game/guessing_game/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+
+    println!("Please input your guess.");
+    let mut guess: String = String::new();
+
+    io::stdin():
+    .read_line(buf:&mut guess)
+}

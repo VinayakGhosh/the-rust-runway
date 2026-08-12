d342f40c2f3c7a9aad0f560a002028ea78ea9487
Vinayak Ghosh <r.vinayakghosh@gmail.com>
2026-08-12 12:39:39 +0530

    created a guessing game in rust

--- Module2 Guessing Game/guessing_game/src/main.rs
+++ Module2 Guessing Game/guessing_game/src/main.rs
@@ -1,11 +1,39 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::RngExt;
+use colored::*;
 
 fn main() {
-    println!("Hello, world!");
 
-    println!("Please input your guess.");
-    let mut guess: String = String::new();
+    let secret_number = rand::rng().random_range(1..=100);
+    let mut count: u32 = 0;
 
-    io::stdin():
-    .read_line(buf:&mut guess)
+    loop{
+        println!("Please input your guess.");
+        let mut guess: String = String::new();
+
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        count+=1;
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less=> println!("{}","Too small".red()),
+            Ordering::Greater=> println!("{}","Too Large".red()),
+            Ordering::Equal=> {
+                println!("{}","You win".green());
+                break;
+            }
+        }
+    }
+    println!("You guessed in {} times", count)
+    
 }

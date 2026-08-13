ddf74498cd2c5b0ecadf2d8569e872f4df47c538
Vinayak Ghosh <r.vinayakghosh@gmail.com>
2026-08-13 12:36:57 +0530

    shadowing

--- Module3 Variables and Mutability/variables/src/main.rs
+++ Module3 Variables and Mutability/variables/src/main.rs
@@ -4,5 +4,16 @@ fn main() {
     println!("Value of x is {}", x);
 
     x=6;
-    println!("Value of x is {}", x)
+    println!("Value of x is {}", x);
+
+    // shadowing
+    let y = 6;
+    println!("Value of Y = {}", y);
+    let y = "six";
+    println!("Value of Y = {}", y);
+
+
+    const SUBSCRIBE_COUNT: u32 = 100000;
+    println!("SUBSCRIBE_COUNT is: {}", SUBSCRIBE_COUNT)
+
 }

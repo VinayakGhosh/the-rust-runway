eb255173abd864034835a6dab5276d067c2c4a87
Vinayak Ghosh <r.vinayakghosh@gmail.com>
2026-08-14 05:39:54 +0530

    basic concepts of ownership

---
+++ Module4 Ownership/ownership/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // -----------Ownership Rules------------
+    // 1) Each value in Rust has a variable that's called it's owner.
+    // 2) There can only be one owner at a time.
+    // 3) When the owner goes out of scope, the value will be dropped.
+
+
+
+    // creating a scope
+    {   // here s is not valid, it's not declared yet!
+        let _s = "Hello";    // s is valid from this point forward
+        // do stuff with s
+    }   // this scope is over now, and s is no longer valid
+
+    // Note that s above is a string literal and it get's directly stored in the binary and is fixed in size.
+
+    // If we want a string dynamic in size and that we can mutate
+    let _s = String::from("Hello");     // now this string is stored in the heap
+}

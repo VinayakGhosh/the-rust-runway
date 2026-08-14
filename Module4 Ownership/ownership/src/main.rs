23dfbd10cf39161a162987cdfcc735a36001a6c9
Vinayak Ghosh <r.vinayakghosh@gmail.com>
2026-08-14 06:27:40 +0530

    how variables and data interact in Rust

--- Module4 Ownership/ownership/src/main.rs
+++ Module4 Ownership/ownership/src/main.rs
@@ -16,4 +16,16 @@ fn main() {
 
     // If we want a string dynamic in size and that we can mutate
     let _s = String::from("Hello");     // now this string is stored in the heap
+
+
+
+
+    // -----------How variables and data interact-----------------
+    let x = 5;
+    let y = x;      // copying value
+
+    let s1 = String::from("hello");
+    let s2 = s1;    // Move (not shallow copy)
+
+    println!("s1 is: {}", s1);  //this line would give error as s1 is not valid anymore 
 }
